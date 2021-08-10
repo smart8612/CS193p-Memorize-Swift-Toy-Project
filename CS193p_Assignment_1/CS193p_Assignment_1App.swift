c25e71bc5b57499a7849d1785658ee42b86c9594
smart8612 <smart8612@gmail.com>
2021-08-10 22:53:32 +0900

    Initial Commit

---
+++ CS193p_Assignment_1/CS193p_Assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  CS193p_Assignment_1App.swift
+//  CS193p_Assignment_1
+//
+//  Created by JeongTaek Han on 2021/08/10.
+//
+
+import SwiftUI
+
+@main
+struct CS193p_Assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS193p_Assignment_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CS193p_Assignment_1
+//
+//  Created by JeongTaek Han on 2021/08/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
